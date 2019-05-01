c090391a0c15b364eba78ecbfde27c85d3606a52
TEWi_R <art.sht.0317@gmail.com>
2019-05-01 16:57:30 +0000

    add tags

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -41,12 +41,21 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to add your article.'));
         }
+        // タグのリストを取得
+        $tags = $this->Articles->Tags->find('list');
+
+        // ビューのコンテキストにtagsをセット
+        $this->set('tags', $tags);
+
         $this->set('article', $article);
     }
 
     public function edit($slug)
     {
-        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $article = $this->Articles->
+        findBySlug($slug)
+        ->contain('Tags') // 関連づけられたTagsを読み込む
+        ->firstOrFail();
         if ($this->request->is(['post', 'put'])) {
             $this->Articles->patchEntity($article, $this->request->getData());
             if ($this->Articles->save($article)) {
@@ -55,6 +64,12 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to update your article.'));
         }
+
+        // タグのリストを取得
+        $tags = $this->Articles->Tags->find('list');
+
+        // ビューコンテキストにtagsをセット
+        $this->set('tags', $tags);
 
         $this->set('article', $article);
     }
